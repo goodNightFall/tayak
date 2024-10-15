8fd037e5cd881704403d4b1e9e85b8b1c2ddd860
Alexey Shabarshov <shabarshov@greensight.ru>
2024-10-15 07:53:19 +0300

    lab1: replaced test expression

--- lab1/src/index.ts
+++ lab1/src/index.ts
@@ -2,4 +2,8 @@ import { Calculator } from "./calculator"
 
 const calculator = new Calculator()
 
-console.log(calculator.calculate("pow(2; 2) - log(2; 16)"))
+console.log(
+    calculator.calculate(
+        "-pow(1 + 1; 1 + 1) + log(3 - 1; pow(2; pow(4 / 2; 2 * 1)))"
+    )
+)
